ea4743961cf4c27eb02707b36572fb83eacbbcb1
lufeirider <3020207744@qq.com>
2017-05-25 12:06:43 +0800

    Add files via upload

---
+++ axfr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#usage:axfr.sh baidu.com
+domain=$1
+nsserver=`dig +short ns $domain`
+echo -e "\033[33m$nsserver\033[0m"
+for ns in $nsserver
+do
+	echo -e "\t正在与服务器$ns尝试区域传送..."
+	ns=${ns:0:-1}
+	echo -e "\tdomain=$domain   nsserver=@$ns"
+	dig @$ns axfr $domain
+done
